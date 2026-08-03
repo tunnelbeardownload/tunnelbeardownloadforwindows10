6dedd103d411487ea654c46d4c06f39c6d234ab3
tunnelbeardownload <shjtrjryjrtjfgadjh@outlook.com>
2026-08-03 15:33:35 +0530

    Add initial Sphinx configuration for documentation

---
+++ docs/conf.py
@@ -0,0 +1,15 @@
+project = 'tunnelbear download for windows 10'
+author = 'Your Name'
+release = '1.0'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+html_js_files = [
+    'chatbot.js',
+]
+html_favicon = '.png'
